9dbd8f6533d7ca8af440eed546a14df8cfe5ba8b
raksa <eng.raksa@gmail.com>
2017-01-14 18:26:21 +0700

    - update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 "use strict";
+var appendFakeMessage;
 var MessageBox = function () {
   this.messageContent = $('#messages-content');
   this.i = 0;
   this.timeI = 0;
   this.messageContent.mCustomScrollbar();
-  var fakeMessages = ['Hi there, I\'m Fabio and you?', 'Nice to meet you', 'How are you?', 'Not too bad, thanks', 'What do you do?', 'That\'s awesome', 'Codepen is a nice place to stay', 'I think you\'re a nice person', 'Why do you think that?', 'Can you explain?', 'Anyway I\'ve gotta go now', 'It was a pleasure chat with you', 'Time to make a new codepen', 'Bye', ':)'];
   this.textAlign = {
     LEFT: 'text-align-left',
     CENTER: 'text-align-center',
@@ -37,9 +37,8 @@ var MessageBox = function () {
     this.setInfo(this.textAlign.RIGHT, (new Date()).toISOString());
     $('.message-input').val(null);
     this.updateScrollbar();
-    var self = this;
     setTimeout(function () {
-      self.appendFakeMessage(fakeMessages[this.i]);
+      appendFakeMessage();
     }, 1000 + (Math.random() * 20) * 100);
   };
 
@@ -47,7 +46,7 @@ var MessageBox = function () {
     this.insertMessage();
   });
   var self = this;
-  $(window).on('keydown', function (e) {
+  $('#message-input').on('keydown', function (e) {
     if (e.which == 13) {
       self.insertMessage();
       return false;
@@ -56,31 +55,31 @@ var MessageBox = function () {
 
   this.loading = function () {
     if ($('#message-input').val() != '') return false;
-    var element = '<div class="message loading new"><figure class="avatar"><img src="image/avatar.png" /></figure><span></span></div>';
+    var element = '<div class="message loading new"><figure class="avatar"><img src="image/avatar-unknown.png" /></figure><span></span></div>';
     $(element).appendTo($('.mCSB_container'));
     this.updateScrollbar();
   };
 
-  this.appendMessage = function () {
+  this.appendMessage = function (message) {
     $('.message.loading').remove();
-    var element = '<div class="message new"><figure class="avatar"><img src="image/avatar.png" /></figure>' + fakeMessages[this.i] + '</div>'
+    var element = '<div class="message new"><figure class="avatar"><img src="image/avatar.png" /></figure>' + message + '</div>'
     $(element).appendTo($('.mCSB_container')).addClass('new');
     this.setInfo(this.textAlign.LEFT, (new Date()).toISOString(), 'sender');
     this.updateScrollbar();
-    this.i++;
-    this.i = this.i >= fakeMessages.length ? 0 : this.i;
   }
-
-  this.appendFakeMessage = function (message) {
-    this.loading();
-    var self = this;
-    setTimeout(() => {
-      self.appendMessage();
-    }, 1000 + (Math.random() * 20) * 100, this);
-  };
 };
 
 $(document).ready(function () {
+  var fakeMessages = ['Hi there, I\'m Fabio and you?', 'Nice to meet you', 'How are you?', 'Not too bad, thanks', 'What do you do?', 'That\'s awesome', 'Codepen is a nice place to stay', 'I think you\'re a nice person', 'Why do you think that?', 'Can you explain?', 'Anyway I\'ve gotta go now', 'It was a pleasure chat with you', 'Time to make a new codepen', 'Bye', ':)'];
+  var i = 0;
   var messageBox = new MessageBox();
-  messageBox.appendFakeMessage();
+  appendFakeMessage = function () {
+    messageBox.loading();
+    setTimeout(() => {
+      messageBox.appendMessage(fakeMessages[i]);
+      i++;
+      i = i >= fakeMessages.length ? 0 : i;
+    }, 1000 + (Math.random() * 20) * 100, this);
+  };
+  appendFakeMessage();
 });
