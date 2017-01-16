1e1d8912207d6d646a13800f898a94242f09351b
raksa <eng.raksa@gmail.com>
2017-01-16 20:32:27 +0700

    - update

--- tab/index.js
+++ tab/index.js
@@ -1,9 +1,24 @@
 "use strict";
 
-function userClick(e) {
-    console.log($(e).attr('data'));
-}
-$(document).ready(function () {
+function ActiveBox(option) {
+    var box = $('#box');
+    var tabBox = $('#tab-box');
+    box.css({
+        'width': option.width + 'px',
+        'height': option.height + 'px'
+    });
+    tabBox.css({
+        'width': (option.width - 60) + 'px',
+        'height': (option.height - 35) + 'px'
+    })
+
+    this._userOnline = $("#box .user-online .numb");
+    this._waitingBoard = $("#box .waiting-board .numb");
+    this._playingBoard = $("#box .playing-board .numb");
+    this._userOnlineContainer = $('.tab.user-online .content');
+    this._waitingBoardContainer = $('.tab.waiting-board .content');
+    this._playingBoardContainer = $('.tab.playing-board .content');
+
     $("#box .side-bar-item").click(function (e) {
         e.preventDefault();
         $('#box .side-bar-item').removeClass("active");
@@ -11,24 +26,85 @@ $(document).ready(function () {
         var data = $(this).attr('data');
         $('.' + data).addClass("active");
     });
-
-    var userOnline = $("#box .user-online .numb");
-    var waitingBoard = $("#box .waiting-board .numb");
-    var playingBoard = $("#box .playing-board .numb");
-    setInterval(function () {
-        userOnline.text(Math.floor(Math.random() * 95) + 5);
-        waitingBoard.text(Math.floor(Math.random() * 95) + 5);
-        playingBoard.text(Math.floor(Math.random() * 95) + 5);
-    }, 3000);
-
-    var tabContent = $('#tab-content');
-    for (var i = 0; i < 70; i++) {
-        var element = '<a href="#" class="user" data="00' + i + '" onclick="userClick(this);">' +
+    this.userOnlineCount = function (n) {
+        if (n != void 0) return Number(this._userOnline.text() || 0);
+        this._userOnline.text(n);
+    };
+    this.waitingBoardCount = function (n) {
+        if (n != void 0) return Number(this._waitingBoard.text() || 0);
+        this._waitingBoard.text(n);
+    };
+    this.playingBoardCount = function (n) {
+        if (n != void 0) return Number(this._playingBoard.text() || 0);
+        this._playingBoard.text(n);
+    };
+    this.addUser = function (user) {
+        var element = '<a href="#" class="user" data="' + user.id + '" onclick="ActiveBox.userOnlineClick(this);">' +
             '<figure class="avatar">' +
-            '<img src="../image/avatar.png" />' +
+            '<img src="' + user.avatar + '" />' +
             '</figure>' +
-            '<span class="name">User 00 asd fa sdf asd f asd f asf as f as fas s fas dfasdf asdf as df asd' + i + '</span>' +
+            '<span class="name">' + user.name + '</span>' +
             '</a>';
-        tabContent.append(element);
+        this._userOnlineContainer.append(element);
+    };
+
+    this.addUserOnlineClickListener = function (listener, context) {
+        ActiveBox._userOnlineClickListeners = ActiveBox._userOnlineClickListeners || [];
+        ActiveBox._userOnlineClickListeners.push([listener, context]);
+    };
+    this.addWaitingBoardClickListener = function (listener, context) {
+        ActiveBox._waitingBoardClickListeners = ActiveBox._waitingBoardClickListeners || [];
+        ActiveBox._waitingBoardClickListeners.push([listener, context]);
+    };
+    this.addPlayingBoardClickListener = function (listener, context) {
+        ActiveBox._playBoardClickListeners = ActiveBox._playBoardClickListeners || [];
+        ActiveBox._playBoardClickListeners.push([listener, context]);
+    };
+}
+
+function callListeners(listeners, e) {
+    listeners.forEach(function (lc) {
+        lc[0].call(lc[1], $(this).attr('data'));
+    }, e);
+}
+ActiveBox.userOnlineClick = function (e) {
+    callListeners(ActiveBox._userOnlineClickListeners || [], e)
+}
+ActiveBox.boardClick = function (e, isWaiting) {
+    callListeners(isWaiting ? ActiveBox._waitingBoardClickListeners || [] : ActiveBox._playingBoardClickListeners || [], e)
+}
+
+$(document).ready(function () {
+    var user = {
+        id: '123',
+        name: 'user',
+        avatar: 'avatar.png'
+    }
+    var activeBox = new ActiveBox({
+        width: 400,
+        height: 600,
+        user: user
+    });
+    activeBox.addUserOnlineClickListener(function (id) {
+        console.log('User id:' + id + ' clicked');
+    }, this)
+    activeBox.addWaitingBoardClickListener(function (id) {
+        console.log('Waiting board id:' + id + ' clicked');
+    }, this)
+    activeBox.addPlayingBoardClickListener(function (id) {
+        console.log('Playing board id:' + id + ' clicked');
+    }, this)
+    setInterval(function () {
+        this.userOnlineCount(Math.floor(Math.random() * 95) + 5);
+        this.waitingBoardCount(Math.floor(Math.random() * 95) + 5);
+        this.playingBoardCount(Math.floor(Math.random() * 95) + 5);
+    }.bind(activeBox), 3000);
+
+    for (var i = 0; i < 70; i++) {
+        activeBox.addUser({
+            id: '00' + i,
+            name: 'User 00' + i,
+            avatar: '../image/avatar.png'
+        })
     }
 });
